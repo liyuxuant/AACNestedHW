2489c565fe180f46e7fdeea973b93e621b788313
Yuxuan Li <tony_li@YuxuandeMacBook-Pro-3.local>
2024-04-21 22:34:05 -0500

    redo

--- src/structures/AACCategory.java
+++ src/structures/AACCategory.java
@@ -10,61 +10,61 @@ package structures;
 public class AACCategory {
 
     //field
-    private String name; 
-    private AssociativeArray<String, String> items; 
+  private String name; 
+  private AssociativeArray<String, String> items; 
     
     //Constructor
-    public AACCategory(String name) {
-        this.name = name;
-        this.items = new AssociativeArray<>(); 
-    }
+  public AACCategory(String name) {
+    this.name = name;
+    this.items = new AssociativeArray<>(); 
+  }
 
     /**
        Adds the mapping of the imageLoc to the text to the category(associative array).
      */
-    public void addItem(String imageLoc, String text) {
+  public void addItem(String imageLoc, String text) {
     
-        try {
-            items.set(imageLoc, text);
-        } catch (NullKeyException e) {
+    try {
+      items.set(imageLoc, text);
+    } catch (NullKeyException e) {
             
-        }
-    }
+      }
+  }
 
     /**
      * Returns the name of the category.
      */
-    public String getCategory() {
-        return this.name; 
-    }
+  public String getCategory() {
+    return this.name; 
+  }
 
     /**
      * Returns the text associated with the given image location in this category.
      */
-    public String getText(String imageLoc) {
-        try {
-            return items.get(imageLoc);
-        } catch (KeyNotFoundException e) {
-            return null;
-        }
+  public String getText(String imageLoc) {
+    try {
+      return items.get(imageLoc);
+    } catch (KeyNotFoundException e) {
+        return null;
     }
+  }
 
     /**
      * Determines if the provided image is stored in the category(associative array).
      */
-    public boolean hasImage(String imageLoc) {
-        // Check if the image location is in the AssociativeArray
-        return items.hasKey(imageLoc);
-    }
+  public boolean hasImage(String imageLoc) {
+    // Check if the image location is in the AssociativeArray
+    return items.hasKey(imageLoc);
+  }
 
     /**
      * Returns an array of all the images in the category.
      */
-    public String[] getImages() {
-        String[] imageLocs = new String[items.size()];
-        for (int i = 0; i < items.size(); i++) {
-            imageLocs[i] = items.pairs[i].key.toString(); 
-        }
-        return imageLocs;
+  public String[] getImages() {
+    String[] imageLocs = new String[items.size()];
+    for (int i = 0; i < items.size(); i++) {
+      imageLocs[i] = items.pairs[i].key.toString(); 
     }
+    return imageLocs;
+  }
 }
--- src/structures/AACMappings.java
+++ src/structures/AACMappings.java
@@ -12,64 +12,64 @@ import java.io.PrintWriter;
 
 
 public class AACMappings {
-    private AssociativeArray<String, AACCategory> cat;
-    private AACCategory currentCat;
+  private AssociativeArray<String, AACCategory> cat;
+  private AACCategory currentCat;
 
-    public AACMappings(String filename) {
-        this.cat = new AssociativeArray<>();
-        this.currentCat = new AACCategory(filename);
+  public AACMappings(String filename) {
+    this.cat = new AssociativeArray<>();
+    this.currentCat = new AACCategory(filename);
+  }
+
+  public void add(String imageLoc, String text) {
+    currentCat.addItem(imageLoc, text);
+  }
+
+  public String getCurrentCategory() {
+    return currentCat.getCategory();
+  }
+
+  public String[] getImageLocs() {
+    return currentCat.getImages();
+  }
+
+  public String getText(String imageLoc) throws KeyNotFoundException {
+    if (isCategory(imageLoc)) {
+      currentCat = cat.get(imageLoc);
+        return null; 
     }
+    return currentCat.getText(imageLoc);
+  }
 
-    public void add(String imageLoc, String text) {
-        currentCat.addItem(imageLoc, text);
-    }
-
-    public String getCurrentCategory() {
-        return currentCat.getCategory();
-    }
-
-    public String[] getImageLocs() {
-        return currentCat.getImages();
-    }
-
-    public String getText(String imageLoc) throws KeyNotFoundException {
-        if (isCategory(imageLoc)) {
-            currentCat = cat.get(imageLoc);
-            return null; 
-        }
-        return currentCat.getText(imageLoc);
-    }
-
-    public void reset() {
+  public void reset() {
         // Reset to default category
-        currentCat = new AACCategory("default");
-    }
+    currentCat = new AACCategory("default");
+  }
 
-    public boolean isCategory(String imageLoc) {
-        try {
-            return cat.hasKey(imageLoc);
-        } catch (Exception e) {
-            return false;
+  public boolean isCategory(String imageLoc) {
+    try {
+      return cat.hasKey(imageLoc);
+    } catch (Exception e) {
+        return false;
+    }
+  }
+    
+    
+  public void writeToFile(String filename) throws IOException {
+    PrintWriter writer = new PrintWriter(new FileWriter(filename));
+      for (int i = 0; i < cat.size(); i++) {
+        KVPair<String, AACCategory> kvPair = cat.pairs[i]; 
+        String categoryKey = kvPair.key;
+        AACCategory category = kvPair.value;
+        writer.println(categoryKey + " " + category.getCategory());
+    
+        for (String imageLoc : category.getImages()) {
+          String text = category.getText(imageLoc);
+
+          writer.println(">" + imageLoc + " " + text);
         }
     }
-    
-    
-    public void writeToFile(String filename) throws IOException {
-        PrintWriter writer = new PrintWriter(new FileWriter(filename));
-            for (int i = 0; i < cat.size(); i++) {
-                KVPair<String, AACCategory> kvPair = cat.pairs[i]; 
-                String categoryKey = kvPair.key;
-                AACCategory category = kvPair.value;
-                writer.println(categoryKey + " " + category.getCategory());
-    
-            for (String imageLoc : category.getImages()) {
-                    String text = category.getText(imageLoc);
-
-                writer.println(">" + imageLoc + " " + text);
-            }
-        }
             
-    }
+  }
 }
 
 
